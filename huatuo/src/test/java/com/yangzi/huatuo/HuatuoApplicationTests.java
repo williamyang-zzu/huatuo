22738d7195128a4bd024e26e8e8fdfa691786283
williamyang-zzu <1654263161@qq.com>
2021-09-17 13:10:25 +0800

    huotuo init

---
+++ huatuo/src/test/java/com/yangzi/huatuo/HuatuoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yangzi.huatuo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HuatuoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
